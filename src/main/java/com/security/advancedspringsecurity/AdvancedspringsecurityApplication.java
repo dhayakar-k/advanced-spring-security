e4ed95b62177d83fd6c19c9a3cbe09e79c06ee75
dhayakardhaya <dhayakar@digitaldots.ai>
2020-12-13 15:16:15 +0530

    Initial Commit

---
+++ src/main/java/com/security/advancedspringsecurity/AdvancedspringsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.security.advancedspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedspringsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedspringsecurityApplication.class, args);
+	}
+
+}
